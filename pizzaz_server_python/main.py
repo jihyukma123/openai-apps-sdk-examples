2e41a946bd47e044c8de25210c5327198373a878
Jeff <t90laser93@gmail.com>
2025-10-22 15:23:00 +0900

    .

--- pizzaz_server_python/main.py
+++ pizzaz_server_python/main.py
@@ -303,6 +303,8 @@ except Exception:
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
